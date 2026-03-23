ecc0a9dfd151de79b4fd0ba60f42fb43b5a50723
Berg Pinheiro <berg.pinheiro2009@gmail.com>
2026-03-23 20:59:00 -0300

    Initial commit: add dashboard build files

---
+++ _nuxt/B2SS2Lvq.js
@@ -0,0 +1,1 @@
+import{as as r}from"./4K6iPk-X.js";var s=r();export{s as O};
---
+++ _nuxt/TFee7LCP.js
@@ -0,0 +1,1 @@
+import r from"./CMFpaLcx.js";import{t,L as o}from"./4K6iPk-X.js";import"./DJe8FK1X.js";import"./B6z5jQEM.js";const i={__name:"AppSidebar",setup(e){return(p,a)=>(t(),o(r))}};export{i as default};
