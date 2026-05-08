b2eccb0aa517e3d1fda62aa9840d199bf83fbf16
Berg Pinheiro <berg.pinheiro2009@gmail.com>
2026-05-08 11:05:47 -0300

    Deploy dashboard: account key permission defaults (delete on by default in UI)

--- _nuxt/fWUzAqkW.js
+++
@@ -1,1 +0,0 @@
-import{au as r}from"./BGiXlm_X.js";var e=r();export{e as O};
---
+++ _nuxt/iYABa0BL.js
@@ -0,0 +1,1 @@
+import r from"./DpvBxIBT.js";import{t,L as o}from"./rJktEnqB.js";import"./wGYRNUNa.js";import"./Csy-fezX.js";const i={__name:"AppSidebar",setup(e){return(p,a)=>(t(),o(r))}};export{i as default};
